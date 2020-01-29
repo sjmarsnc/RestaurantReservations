906b5394bd4457741f1f79bec4fd916dcd08b84d
sjmarsnc <sjmarsnc@gmail.com>
2020-01-29 09:25:44 -0500

    changed to print out port being used

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var http = require("http");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -111,6 +111,6 @@ app.post("/api/reserve", function(req, res)  {
   // Starts the server to begin listening
   // =============================================================
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log(`App listening on PORT ${PORT}`);
   });
   
